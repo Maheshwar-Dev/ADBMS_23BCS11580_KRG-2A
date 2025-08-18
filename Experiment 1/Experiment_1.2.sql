0c54abc3ae9b0f5ef224cfc86706b15e75466878
Maheshwar <146438197+Maheshwar-Dev@users.noreply.github.com>
2025-08-18 10:10:03 +0530

    Update Experiment_1.2.sql

--- Experiment 1/Experiment_1.2.sql
+++ Experiment 1/Experiment_1.2.sql
@@ -1,27 +1,45 @@
-CREATE TABLE Employees(
-  EmpID INT PRIMARY KEY,
-  EName VARCHAR(50),
-  Department VARCHAR(50),
-  ManagerID INT 
+CREATE TABLE Departments (
+    DeptID INT PRIMARY KEY,
+    DeptName VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE Courses (
+    CourseID INT PRIMARY KEY,
+    CourseName VARCHAR(100) NOT NULL,
+    DeptID INT FOREIGN KEY REFERENCES Departments(DeptID)
 );
 
 
-
-ALTER TABLE Employees ADD CONSTRAINT fKey FOREIGN KEY(ManagerID) REFERENCES Employees(EmpID);
-
-
-
-INSERT INTO Employees VALUES (1,'Akshat', 'CSE', NULL), 
-                             (2,'Vaibhav', 'ECE', 1), 
-                             (3,'Gopal', 'CSE', 1), 
-                             (4,'Ram', 'EE', 3), 
-                             (5,'Geeta', 'ME', 2), 
-                             (6,'Naman', 'ECE', 3);
+INSERT INTO Departments (DeptID,DeptName) VALUES
+(1,'Computer Science'),
+(2,'Mechanical'),
+(3,'Civil'),
+(4,'Electrical'),
+(5,'Mechatronics');
 
 
-SELECT A.Ename AS [Employee Name], B.EName AS [Manager Name]
-FROM Employees AS A
-LEFT OUTER JOIN
-Employees AS B
-ON 
-A.ManagerID = B.EmpID;
+INSERT INTO Courses (CourseID, CourseName, DeptID) VALUES
+(101,'Data Structures', 1),
+(102,'DBMS', 1),
+(103,'Operating System', 1),
+(104,'Fluid Mechanics', 2),
+(105,'Workshop', 2),
+(106,'Highway Engineering', 3),
+(107,'RCC', 3),
+(108,'Digital Electronics', 4),
+(109,'Alternating Current', 4),
+(110,'Robotics', 5),
+(111,'AI', 5);
+
+
+SELECT d.DeptName,
+       (SELECT COUNT(*) 
+        FROM Courses c 
+        WHERE c.DeptID = d.DeptID) AS CourseCount
+FROM Departments d;
+
+SELECT d.DeptName
+FROM Departments d
+WHERE (SELECT COUNT(*) 
+       FROM Courses c 
+       WHERE c.DeptID = d.DeptID) > 2;
